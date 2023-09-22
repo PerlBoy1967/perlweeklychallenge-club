4a827f311d1a34ce4be45898a367f2af462e05c4
Niels van Dijke <perlboy@cpan.org>
2023-09-22 15:19:13 +0000

    Task #1 revised

--- challenge-235/perlboy1967/perl/ch1.pl
+++ challenge-235/perlboy1967/perl/ch1.pl
@@ -25,12 +25,28 @@ use Test::More;
 use List::MoreUtils qw(slide);
 
 sub removeOne (@) {
-  scalar(grep { $_ < 0 } slide { $b - $a } @_) <= 1 ? 1 : 0;
+  for my $n (1,2) {
+    # Find delta < 0
+    my @d = slide { $b - $a } @_;
+
+    # Find indexes of delta < 0
+    my $i = -1;
+    my @n = map { $i++; $_ < 0 ? ($i) : () } @d;
+
+    # Return if too many violator(s) in iteration
+    return 0 if (scalar(@n) > 1 or ($n == 2 and scalar(@n) > 0));
+
+    # Remove the violating item 
+    splice(@_,$n[0],1);
+  }
+
+  return 1;
 }
 
 is(removeOne(0, 2, 9, 4, 6), 1);
 is(removeOne(5, 1, 3, 2), 0);
 is(removeOne(2, 2, 3), 1);
+is(removeOne(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3), 0);
 
 done_testing;
 
